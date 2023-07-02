0814aac5673ca2ac752d92303e1191da9d90649f
ov7a <ov7a@yandex.ru>
2023-07-02 16:43:23 +0400

    Fix karma tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,7 +43,7 @@ kotlin {
 
             testTask {
                 useKarma {
-                    useFirefoxHeadless()
+                    useChromiumHeadless()
                 }
             }
         }
