ae4fcccae15d635fc52ee15e297caba72aa4b61d
ov7a <ov7a@yandex.ru>
2023-07-17 23:58:19 +0400

    minor: refactor element getters

--- src/main/kotlin/ru/ov7a/github/insights/ui/elements/utils.kt
+++ src/main/kotlin/ru/ov7a/github/insights/ui/elements/utils.kt
@@ -5,13 +5,13 @@ import org.w3c.dom.HTMLElement
 import org.w3c.dom.HTMLInputElement
 import org.w3c.dom.HTMLSelectElement
 
-fun getInput(inputId: String): HTMLInputElement =
-    document.getElementById(inputId) as HTMLInputElement?
-        ?: throw RuntimeException("Oopsie, can't find input $inputId by id")
+fun getInput(id: String): HTMLInputElement =
+    getHtmlElement(id) as? HTMLInputElement?
+        ?: throw RuntimeException("Element $id is not input")
 
-fun getSelector(inputId: String): HTMLSelectElement =
-    document.getElementById(inputId) as HTMLSelectElement?
-        ?: throw RuntimeException("Oopsie, can't find select $inputId by id")
+fun getSelector(id: String): HTMLSelectElement =
+    getHtmlElement(id) as? HTMLSelectElement?
+        ?: throw RuntimeException("Element $id is not select")
 
 fun getHtmlElement(id: String): HTMLElement =
     document.getElementById(id) as HTMLElement? ?: throw RuntimeException("Element not found: $id")
