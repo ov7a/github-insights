a15bce99c16a4b6c82315ada0ecbe7aec4cafff2
ov7a <ov7a@yandex.ru>
2023-07-17 23:38:02 +0400

    Diplay login for auth

--- src/main/kotlin/ru/ov7a/github/insights/ui/contexts/AuthorizationContext.kt
+++ src/main/kotlin/ru/ov7a/github/insights/ui/contexts/AuthorizationContext.kt
@@ -12,6 +12,8 @@ class AuthorizationContext {
     private val userIdInput by lazy { getInput(USER_INPUT_ID) }
     private val tokenIdInput by lazy { getInput(TOKEN_INPUT_ID) }
 
+    private val loginHint by lazy { getHtmlElement(LOGIN_ID) }
+
     private val authorizedBlock by lazy { getHtmlElement(AUTHORIZED_BLOCK_ID) }
     private val unauthorizedBlock by lazy { getHtmlElement(UNAUTHORIZED_BLOCK_ID) }
 
@@ -31,16 +33,21 @@ class AuthorizationContext {
         }
         val authorizationHeader: String = constructBasicAuthValue(user, token)
         Cookies.set(AUTH_DATA_COOKIE, authorizationHeader, cookieAttrs)
+        Cookies.set(USER_COOKIE, user, cookieAttrs)
         updateVisibility()
     }
 
     fun resetAuthorization() {
         Cookies.remove(AUTH_DATA_COOKIE, cookieAttrs)
+        Cookies.remove(USER_COOKIE, cookieAttrs)
         updateVisibility()
     }
 
     private fun updateVisibility() {
         val authorized = (getAuthorization() != null)
+
+        val user = Cookies.get(USER_COOKIE)
+        loginHint.textContent = user
 
         authorizedBlock.hidden = !authorized
         unauthorizedBlock.hidden = authorized
@@ -50,9 +57,11 @@ class AuthorizationContext {
         @Suppress("UNCHECKED_CAST_TO_EXTERNAL_INTERFACE")
         val cookieAttrs = json("secure" to true, "sameSite" to "None") as CookieAttributes
         const val AUTH_DATA_COOKIE = "AuthData"
+        const val USER_COOKIE = "User"
 
         const val USER_INPUT_ID = "auth_user"
         const val TOKEN_INPUT_ID = "auth_token"
+        const val LOGIN_ID = "login"
 
         const val AUTHORIZED_BLOCK_ID = "authorized"
         const val UNAUTHORIZED_BLOCK_ID = "unauthorized"
