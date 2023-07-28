3be8e30eb8aca3ad0f75881d9ae9376399955841
ov7a <ov7a@yandex.ru>
2023-07-28 12:48:57 +0400

    migrate to kotlin multiplatform

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     // 1.8.22+ has type generation disabled: https://youtrack.jetbrains.com/issue/KT-54445
-    kotlin("js") version "1.8.10"
+    kotlin("multiplatform") version "1.8.10"
     kotlin("plugin.serialization") version "1.8.10"
 }
 
@@ -15,30 +15,6 @@ val dateTimeVersion = "0.4.0"
 val jsCookieVersion = "2.2.1" // 3.0+ has problems with generated definitions
 val kotestVersion = "5.6.2"
 
-dependencies {
-    implementation(kotlin("stdlib-js"))
-    implementation(kotlin("reflect"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:$coroutinesVersion")
-
-    implementation("org.jetbrains.kotlinx:kotlinx-html:$kotlinxHtmlVersion")
-    implementation("org.jetbrains.kotlinx:kotlinx-html-js:$kotlinxHtmlVersion")
-
-    implementation("io.ktor:ktor-client-core:$ktorVersion")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
-    implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
-
-    implementation("org.jetbrains.kotlinx:kotlinx-datetime:$dateTimeVersion")
-
-    implementation(npm("js-cookie", jsCookieVersion))
-    implementation(npm("@types/js-cookie", jsCookieVersion, generateExternals = true))
-
-    testImplementation(kotlin("test-js"))
-    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
-    testImplementation("io.kotest:kotest-assertions-core-js:$kotestVersion")
-    testImplementation("io.ktor:ktor-client-mock:$ktorVersion")
-}
-
 kotlin {
     js(IR) {
         browser {
@@ -51,5 +27,37 @@ kotlin {
             }
         }
         useCommonJs()
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib"))
+                implementation(kotlin("reflect"))
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+
+                implementation("org.jetbrains.kotlinx:kotlinx-html:$kotlinxHtmlVersion")
+
+                implementation("io.ktor:ktor-client-core:$ktorVersion")
+                implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+                implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+
+                implementation("org.jetbrains.kotlinx:kotlinx-datetime:$dateTimeVersion")
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+                implementation("io.kotest:kotest-assertions-core:$kotestVersion")
+                implementation("io.ktor:ktor-client-mock:$ktorVersion")
+            }
+        }
+        val jsMain by getting {
+            dependencies {
+                implementation(npm("js-cookie", jsCookieVersion))
+                implementation(npm("@types/js-cookie", jsCookieVersion, generateExternals = true))
+            }
+        }
+        val jsTest by getting
     }
 }
